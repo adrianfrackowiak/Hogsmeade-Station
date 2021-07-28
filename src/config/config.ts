579a75ca4d1dbc47a7adaa55ba42ecfb502d811b
adrianfrackowiak <adrianfrackowiak@icloud.com>
2021-07-28 16:48:34 +0200

    add env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,13 @@
 const config = {
-  firebase: {
-    apiKey: "AIzaSyDRwP0PyQ7NDYaH9kh1xb7N75dJWc0GVmw",
-    authDomain: "harrypotter-2044b.firebaseapp.com",
-    databaseURL:
-      "https://harrypotter-2044b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "harrypotter-2044b",
-    storageBucket: "harrypotter-2044b.appspot.com",
-    messagingSenderId: "806148876127",
-    appId: "1:806148876127:web:4182716c75a72828d4f8ea",
-  },
+    firebase: {
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    },
 };
 
 export default config;
